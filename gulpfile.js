8fae7c81f1bc90177e7336870746e954a99db8be
Sam Smith <sam@samsmith.me>
2017-08-22 22:12:59 +0100

    Tidy up and added new content

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@ var sass        = require('gulp-sass');
 var cleanCSS    = require('gulp-clean-css');
 var sourcemaps  = require('gulp-sourcemaps');
 var rename      = require('gulp-rename');
+var uglify      = require('gulp-uglify');
 var imagemin    = require('gulp-imagemin');
 
 // Create sourcemaps and minify the scss
 gulp.task('styles', function() {
-  gulp.src('src/**/*.scss')
+  gulp.src('src/styles/lib.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(cleanCSS())
@@ -24,16 +25,27 @@ gulp.task('html', function() {
 
 // Optimize images
 gulp.task('images', function() {
-  gulp.src('src/**/*')
+  gulp.src('src/docSite/img/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest('build'));
+});
+
+// Minify Javascript
+gulp.task('scripts', function() {
+  gulp.src('src/scripts/lib.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(rename({suffix : '.min'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build'));
 });
 
 // Watch task
 gulp.task('watch', function() {
-  gulp.watch('src/**/*.scss', ['styles']);
-  gulp.watch('src/**/*.html', ['html']);
-  gulp.watch('src/**/*', ['images']);
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+  gulp.watch('src/docSite/**/*.html', ['html']);
+  gulp.watch('src/docSite/img/**/*', ['images']);
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
 });
 
 // Default task
---
+++ src/scripts/lib.js
@@ -0,0 +1,193 @@
+// JavaScript Framework
+// Author: Sam Smith (smithymx67)
+
+////////////////////////////////////////////////////////////////
+// Element Selector                                           //
+////////////////////////////////////////////////////////////////
+
+/**
+* Return the element passed by its ID
+* @param  {string} element
+* @return {Object} theElement
+**/
+function elem(element) {
+  var theElement;
+  
+  if(element.startsWith('.')) {
+    theElement = document.getElementsByClassName(element.split('.')[1]);
+  } else if(element.startsWith('#')) {
+    theElement = document.getElementById(element.split('#')[1]);
+  } else {
+    theElement = document.getElementsByTagName(element);
+  }
+  
+  return theElement;
+}
+
+////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////
+// Date Time Functions                                        //
+////////////////////////////////////////////////////////////////
+
+/**
+* Convert the month number into text
+* @param  {number} month
+* @return {string}
+**/
+function textifyMonth(month) {
+  var months = [
+    "January", 
+    "Febuary", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"
+  ];
+  return months[month];
+}
+
+/** 
+* Convert the day number into text
+* @param  {number} day
+* @return {string}
+**/
+function textifyWeekDay(day) {
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  return days[day];
+}
+
+/**
+* Returns the time in 12hr format
+* @param  {Date}    dateObj
+* @param  {bool}    showSecs
+* @param  {bool}    showAMPM
+* @return {string}  theTime
+**/
+function get12HrTime(dateObj, showSecs, showAMPM) {
+  var hours = (dateObj.getHours() > 12) ? dateObj.getHours() - 12 : dateObj.getHours();
+  var mins = leadZero(dateObj.getMinutes());
+  var secs = leadZero(dateObj.getSeconds());
+  var ampm = (dateObj.getHours() > 12) ? "PM" : "AM";
+  var theTime;
+  
+  theTime = hours + ":" + mins;
+  if(showSecs === true) {theTime += ":" + secs};
+  if(showAMPM === true) {theTime += " " + ampm};
+  return theTime;
+}
+
+/**
+* Returns the time in 24hr format
+* @param  {Date}    dateObj
+* @param  {bool}    showSecs    
+* @return {string}  theTime
+**/
+function get24HrTime(dateObj, showSecs) {
+  var hours = leadZero(dateObj.getHours());
+  var mins = leadZero(dateObj.getMinutes());
+  var secs = leadZero(dateObj.getSeconds());
+  var theTime;
+  
+  theTime = hours + ":" + mins;
+  if(showSecs === true) {theTime += ":" + secs};
+  return theTime;
+}
+
+/** 
+* Add a leading zero is needed
+* @param  {number} number
+* @return {string}
+**/
+function leadZero(number) {
+  return (number < 10) ? "0" + number : number;
+}
+
+/**
+* Returns the date in the format DD/MM/YYYY
+* @param  {Date}    dateObj
+* @return {string}
+**/
+function getShortDate(dateObj) {
+  var date = leadZero(dateObj.getDate());
+  var month = leadZero(dateObj.getMonth() + 1);
+  var year = dateObj.getFullYear();
+  return date + "/" + month + "/" + year;
+}
+
+/**
+* Return the date in the format Day Date Month Year
+* @param  {Date}    dateObj
+* @return {string}
+**/
+function getLongDate(dateObj) {
+  var day = textifyWeekDay(dateObj.getDay());
+  var date = suffixDate(dateObj.getDate());
+  var month = textifyMonth(dateObj.getMonth());
+  var year = dateObj.getFullYear();
+  return day + " " + date + " " + month + " " + year;
+}
+
+/** 
+* Add a suffix to the date 
+* @param  {number} date
+* @return {string} theDate
+**/
+function suffixDate(date) {
+  var theDate;
+  switch(date) {
+    case 1, 21, 31:
+      theDate = date + "st";
+      break;
+    case 2, 22, 32:
+      theDate = date + "nd";
+      break;
+    case 3, 23:
+      theDate = date + "rd";
+      break;
+    default:
+      theDate = date + "th";
+  }
+  return theDate;
+}
+
+////////////////////////////////////////////////////////////////
+
+/** 
+* Log text to the console
+* @param {string} message
+* @param {string} style
+**/
+function consoleLog(message, style) {
+  switch(style) {
+    case "info":
+      console.log("%c" + message, "color: blue");
+      break;
+    case "success":
+      console.log("%c" + message, "color: green");
+      break;
+    case "warning":
+      console.log("%c" + message, "color: orange");
+      break;
+    case "danger":
+      console.log("%c" + message, "color: red");
+      break;
+    default:
+      console.log(message);
+  }
+}
