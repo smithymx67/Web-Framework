b7364963d1051f689faaac5687746452e4e03efd
Sam Smith <sam@samsmith.me>
2017-08-23 22:45:19 +0100

    Added more features Sticky and fixed footer Position styles

--- src/scripts/lib.js
+++ src/scripts/lib.js
@@ -168,6 +168,45 @@ function suffixDate(date) {
 
 ////////////////////////////////////////////////////////////////
 
+
+////////////////////////////////////////////////////////////////
+// Footer Scripts                                             //
+////////////////////////////////////////////////////////////////
+
+/**
+ * Fix a footer to the bottom of the page
+ */
+window.onload = fixifyFooter();
+function fixifyFooter() {
+  var footer = document.getElementsByTagName("footer");
+
+  for(var i = 0; i < footer.length; i++) {
+    if(footer[i].classList.contains("fixed-footer")) {
+      var offset = footer[i].clientHeight;
+      document.getElementsByTagName("body")[0].style.marginBottom = offset + "px";
+    }
+  }
+}
+
+/**
+ * Stick a footer to the bottom of the page
+ */
+window.onload = stickifyFooter();
+function stickifyFooter() {
+  var footer = document.getElementsByTagName("footer");
+
+  for(var i = 0; i < footer.length; i++) {
+    if(footer[i].classList.contains("sticky-footer")) {
+      var offset = footer[i].clientHeight;
+      document.getElementsByTagName("body")[0].style.paddingBottom = offset + "px";
+      document.getElementsByTagName("html")[0].style.minHeight = "100%";
+      document.getElementsByTagName("html")[0].style.position = "relative";
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////
+
 /** 
 * Log text to the console
 * @param {string} message
