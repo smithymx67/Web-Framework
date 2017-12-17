8190fea34401f5a485e77b2433fede017448c62d
Sam Smith (smithymx67) <sam@samsmith.me>
2017-12-17 18:15:44 +0000

    Margin and padding classes

--- build/docSite/styles/layout.php
+++ build/docSite/styles/layout.php
@@ -294,8 +294,27 @@
     </div>
   </div>
 
-  <div class="col-12">
-    <h2 id=""></h2>
+  <div class="container">
+    <div class="col-12">
+      <h2 id="margin-padding">Margin and Padding Classes</h2>
+      <p>Added v1.1.2</p>
+      <p class="width-6 width-xl-12">These classes can be used to control the padding and margin on elements.</p>
+      <ul>
+        <li><code>margin-0</code></li>
+        <li><code>margin-5</code></li>
+        <li><code>margin-10</code></li>
+        <li><code>margin-50</code></li>
+        <li><code>margin-100</code></li>
+      </ul>
+      <br>
+      <ul>
+        <li><code>padding-0</code></li>
+        <li><code>padding-5</code></li>
+        <li><code>padding-10</code></li>
+        <li><code>padding-50</code></li>
+        <li><code>padding-100</code></li>
+      </ul>
+    </div>
   </div>
   
   <!-- Full Width -->
