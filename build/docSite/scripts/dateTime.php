c27aa60349875ed5628040e1d14e130e9b3cc633
Sam Smith <smithymx67@gmail.com>
2018-02-04 23:46:33 +0000

    More v1.2.0 updates

--- build/docSite/scripts/dateTime.php
+++ build/docSite/scripts/dateTime.php
@@ -1,134 +1,208 @@
 <?php require_once "../config.php"; ?>
 <!DOCTYPE html>
 <html lang="en-GB">
-<head>
-  <title>Web Framework | Scripts | Date Time</title>
-  <?php require_once "../includes/header.php"; ?>
-</head>
-<body>
-  <header>
+  <head>
+    <title>Web Framework | Scripts | Date Time</title>
+    <?php require_once "../includes/header.php"; ?>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="col-12">
+          <h1>Web Framework - Scripts - Date Time</h1>
+        </div>
+      </div>
+    </header>
+
     <div class="container">
       <div class="col-12">
-        <h1>Web Framework - Scripts - Date Time</h1>
+        <div class="header-text">
+          <h1>Date Time</h1>
+          <p>Functions that help with date and time handling.</p>
+        </div>
+      </div>
+
+      <div class="col-12">
+        <a href="<?php echo ROOT_DOCSITE_URL . "/scripts/"; ?>" title="Scripts">Go Back</a>
+      </div>
+
+      <div class="col-12">
+        <h2><code>textifyMonth(<i>[int]month</i>)</code></h2>
+        <p>This function takes in a month number and returns the months name.</p>
+        <p>Valid month numbers are 0 - 11 where 0 returns January and 11 returns December.</p>
+
+        <p>Added v1.0.0</p>
+        <p>Removed v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Output: March</span><br>
+          <span>textifyMonth(2);</span>
+        </div>
+        <br>
+        <p>Added v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Output: March</span><br>
+          <span>dateTime.textifyMonth(2);</span>
+        </div>
+
+        <br><br><br>
+
+        <h2><code>textifyWeekDay(<i>[int]day</i>)</code></h2>
+        <p>This function takes in a day number and returns the days name.</p>
+        <p>Valid day numbers are 0 - 6 where 0 returns Sunday and 6 returns Saturday.</p>
+
+        <p>Added v1.0.0</p>
+        <p>Removed v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Output: Thursday</span><br>
+          <span>textifyWeekDay(4);</span>
+        </div>
+        <br>
+        <p>Added v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Output: Thursday</span><br>
+          <span>dateTime.textifyWeekDay(4);</span>
+        </div>
+
+        <br><br><br>
+
+        <h2><code>get12HrTime(<i>[Date]dateObj</i>, <i>[bool]showSecs</i>, <i>[bool]showAMPM</i>)</code></h2>
+        <p>This function takes in a date object and two boolean values. It returns the time in 12 hour format.</p>
+        <p>Added v1.0.0</p>
+        <p>Removed v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Example output: 7:37</span><br>
+          <span>get12HrTime(new Date(), false, false);</span><br>
+          <br>
+          <span>// Example output: 7:37 PM</span><br>
+          <span>get12HrTime(new Date(), false, true);</span><br>
+          <br>
+          <span>// Example output: 7:37:30</span><br>
+          <span>get12HrTime(new Date(), true, false);</span><br>
+          <br>
+          <span>// Example output: 7:37:30 PM</span><br>
+          <span>get12HrTime(new Date(), true, true);</span>
+        </div>
+        <br>
+        <p>Added v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Example output: 7:37</span><br>
+          <span>dateTime.get12HrTime(new Date(), false, false);</span><br>
+          <br>
+          <span>// Example output: 7:37 PM</span><br>
+          <span>dateTime.get12HrTime(new Date(), false, true);</span><br>
+          <br>
+          <span>// Example output: 7:37:30</span><br>
+          <span>dateTime.get12HrTime(new Date(), true, false);</span><br>
+          <br>
+          <span>// Example output: 7:37:30 PM</span><br>
+          <span>dateTime.get12HrTime(new Date(), true, true);</span>
+        </div>
+
+        <br><br><br>
+
+        <h2><code>get24HrTime(<i>[Date]dateObj</i>, <i>[bool]showSecs</i>)</code></h2>
+        <p>This function takes in a date object and a boolean value. It returns the time in 24 hour format.</p>
+        <p>Added v1.0.0</p>
+        <p>Removed v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Example output: 18:28</span><br>
+          <span>get24HrTime(new Date(), false);</span><br>
+          <br>
+          <span>// Example output: 18:28:12</span><br>
+          <span>get24HrTime(new Date(), true);</span>
+        </div>
+        <br>
+        <p>Added v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Example output: 18:28</span><br>
+          <span>dateTime.get24HrTime(new Date(), false);</span><br>
+          <br>
+          <span>// Example output: 18:28:12</span><br>
+          <span>dateTime.get24HrTime(new Date(), true);</span>
+        </div>
+
+        <br><br><br>
+
+        <h2><code>getShortDate(<i>[Date]dateObj</i>)</code></h2>
+        <p>This function takes in a date object and returns the date in the following format DD/MM/YYYY.</p>
+        <p>Added v1.0.0</p>
+        <p>Removed v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Example output: 21/04/2017</span><br>
+          <span>getShortDate(new Date());</span>
+        </div>
+        <br>
+        <p>Added v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Example output: 21/04/2017</span><br>
+          <span>dateTime.getShortDate(new Date());</span>
+        </div>
+
+        <br><br><br>
+
+        <h2><code>getLongDate(<i>[Date]dateObj</i>)</code></h2>
+        <p>This function takes in a date object and returns the date in the following format Day Date Month Year.</p>
+        <p>Added v1.0.0</p>
+        <p>Removed v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Example output: Saturday 19th August 2017</span><br>
+          <span>getLongDate(new Date());</span>
+        </div>
+        <br>
+        <p>Added v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// Example output: Saturday 19th August 2017</span><br>
+          <span>dateTime.getLongDate(new Date());</span>
+        </div>
+
+        <br><br><br>
+
+        <h2><code>leadZero(<i>[int] number</i>)</code></h2>
+        <p>This function takes in a number and returns it with a leading zero is needed.</p>
+        <p>Added v1.0.0</p>
+        <p>Removed v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// 3 => 03</span><br>
+          <span>leadZero(3);</span>
+        </div>
+        <br>
+        <p>Added v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// 3 => 03</span><br>
+          <span>dateTime.leadZero(3);</span>
+        </div>
+
+        <br><br><br>
+
+        <h2><code>suffixDate(<i>[int] date</i>)</code></h2>
+        <p>This function takes in a number and returns it with the correct suffix (st, nd, rd or th)</p>
+        <p>Added v1.0.0</p>
+        <p>Removed v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// 1 => 1st, 2 => 2nd, 3 => 3rd, 4 => 4th</span><br>
+          <span>suffixDate(2)</span>
+        </div>
+        <br>
+        <p>Added v1.2.0</p>
+        <div class="code-block width-6 width-l-12">
+          <span>// 1 => 1st, 2 => 2nd, 3 => 3rd, 4 => 4th</span><br>
+          <span>dateTime.suffixDate(2)</span>
+        </div>
       </div>
     </div>
-  </header>
-  
-  <div class="container">
-    <div class="col-12">
-      <div class="header-text">
-        <h1>Date Time</h1>
-        <p>Functions that help with date and time handling.</p>
-      </div>
-    </div>
-    
-    <div class="col-12">
-      <a href="<?php echo ROOT_DOCSITE_URL . "/scripts/"; ?>" title="Scripts">Go Back</a>
-    </div>
-    
-    <div class="col-12">
-      <h2><code>textifyMonth(<i>[int]month</i>)</code></h2>
-      <p>Added v1.0.0</p>
-      <p>This function takes in a month number and returns the months name.</p>
-      <p>Valid month numbers are 0 - 11 where 0 returns January and 11 returns December.</p>
-      <div class="code-block width-6 width-l-12">
-        <span>// Output: March</span><br>
-        <span>textifyMonth(2);</span>
-      </div>
-      
-      <br>
-      
-      <h2><code>textifyWeekDay(<i>[int]day</i>)</code></h2>
-      <p>Added v1.0.0</p>
-      <p>This function takes in a day number and returns the days name.</p>
-      <p>Valid day numbers are 0 - 6 where 0 returns Sunday and 6 returns Saturday.</p>
-      <div class="code-block width-6 width-l-12">
-        <span>// Output: Thursday</span><br>
-        <span>textifyWeekDay(4);</span>
-      </div>
-      
-      <br>
-      
-      <h2><code>get12HrTime(<i>[Date]dateObj</i>, <i>[bool]showSecs</i>, <i>[bool]showAMPM</i>)</code></h2>
-      <p>Added v1.0.0</p>
-      <p>This function takes in a date object and two boolean values. It returns the time in 12 hour format.</p>
-      <div class="code-block width-6 width-l-12">
-        <span>// Example output: 7:37</span><br>
-        <span>get12HrTime(new Date(), false, false);</span><br>
-        <br>
-        <span>// Example output: 7:37 PM</span><br>
-        <span>get12HrTime(new Date(), false, true);</span><br>
-        <br>
-        <span>// Example output: 7:37:30</span><br>
-        <span>get12HrTime(new Date(), true, false);</span><br>
-        <br>
-        <span>// Example output: 7:37:30 PM</span><br>
-        <span>get12HrTime(new Date(), true, true);</span>
-      </div>
-      
-      <br>
-      
-      <h2><code>get24HrTime(<i>[Date]dateObj</i>, <i>[bool]showSecs</i>)</code></h2>
-      <p>Added v1.0.0</p>
-      <p>This function takes in a date object and a boolean value. It returns the time in 24 hour format.</p>
-      <div class="code-block width-6 width-l-12">
-        <span>// Example output: 18:28</span><br>
-        <span>get24HrTime(new Date(), false);</span><br>
-        <br>
-        <span>// Example output: 18:28:12</span><br>
-        <span>get24HrTime(new Date(), true);</span>
-      </div>
-      
-      <br>
-      
-      <h2><code>getShortDate(<i>[Date]dateObj</i>)</code></h2>
-      <p>Added v1.0.0</p>
-      <p>This function takes in a date object and returns the date in the following format DD/MM/YYYY.</p>
-      <div class="code-block width-6 width-l-12">
-        <span>// Example output: 21/04/2017</span><br>
-        <span>getShortDate(new Date());</span>
-      </div>
-      
-      <br>
-      
-      <h2><code>getLongDate(<i>[Date]dateObj</i>)</code></h2>
-      <p>Added v1.0.0</p>
-      <p>This function takes in a date object and returns the date in the following format Day Date Month Year.</p>
-      <div class="code-block width-6 width-l-12">
-        <span>// Example output: Saturday 19th August 2017</span><br>
-        <span>getLongDate(new Date());</span>
-      </div>
-      
-      <br>
-      
-      <h2><code>leadZero(<i>[int] number</i>)</code></h2>
-      <p>Added v1.0.0</p>
-      <p>This function takes in a number and returns it with a leading zero is needed.</p>
-      <div class="code-block width-6 width-l-12">
-        <span>// 3 => 03</span><br>
-        <span>leadZero(3);</span>
-      </div>
-      
-      <br>
-      
-      <h2><code>suffixDate(<i>[int] date</i>)</code></h2>
-      <p>Added v1.0.0</p>
-      <p>This function takes in a number and returns it with the correct suffix (st, nd, rd or th)</p>
-      <div class="code-block width-6 width-l-12">
-        <span>// 1 => 1st, 2 => 2nd, 3 => 3rd, 4 => 4th</span><br>
-        <span>suffixDate(2)</span>
-      </div>
-    </div>
-  </div>
-  
-  <?php require_once "../includes/footer.php"; ?>
-  <script>
-    consoleLog(textifyMonth(2), "default");
-    consoleLog(textifyWeekDay(4), "default");
-    consoleLog(get12HrTime(new Date(), true, true), "default");
-    consoleLog(get24HrTime(new Date(), true), "default");
-    consoleLog(getShortDate(new Date()), "default");
-    consoleLog(getLongDate(new Date()), "default");
-  </script>
-</body>
+
+    <?php require_once "../includes/footer.php"; ?>
+    <script>
+      consoleLog.normal(dateTime.textifyMonth(2));
+      consoleLog.normal(dateTime.textifyWeekDay(4));
+      consoleLog.normal(dateTime.get12HrTime(new Date(), true, true));
+      consoleLog.normal(dateTime.get12HrTime(new Date(), true, false));
+      consoleLog.normal(dateTime.get12HrTime(new Date(), false, true));
+      consoleLog.normal(dateTime.get12HrTime(new Date(), false, false));
+      consoleLog.normal(dateTime.get24HrTime(new Date(), true));
+      consoleLog.normal(dateTime.get24HrTime(new Date(), false));
+      consoleLog.normal(dateTime.getShortDate(new Date()));
+      consoleLog.normal(dateTime.getLongDate(new Date()));
+    </script>
+  </body>
 </html>
--- src/scripts/dateTime.js
+++ src/scripts/dateTime.js
@@ -2,144 +2,157 @@
 // Date Time Functions                                        //
 ////////////////////////////////////////////////////////////////
 
-/**
- * Convert the month number into text
- * @param  {number} month
- * @return {string}
- **/
-function textifyMonth(month) {
-  var months = [
-    "January",
-    "Febuary",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-  return months[month];
-}
-
-/**
- * Convert the day number into text
- * @param  {number} day
- * @return {string}
- **/
-function textifyWeekDay(day) {
-  var days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  return days[day];
-}
-
-/**
- * Returns the time in 12hr format
- * @param  {Date}        dateObj
- * @param  {boolean}     showSecs
- * @param  {boolean}     showAMPM
- * @return {string}      theTime
- **/
-function get12HrTime(dateObj, showSecs, showAMPM) {
-  var hours = (dateObj.getHours() > 12) ? dateObj.getHours() - 12 : dateObj.getHours();
-  var mins = leadZero(dateObj.getMinutes());
-  var secs = leadZero(dateObj.getSeconds());
-  var ampm = (dateObj.getHours() > 12) ? "PM" : "AM";
-  var theTime;
-
-  theTime = hours + ":" + mins;
-  if(showSecs === true) {theTime += ":" + secs}
-  if(showAMPM === true) {theTime += " " + ampm}
-  return theTime;
-}
-
-/**
- * Returns the time in 24hr format
- * @param  {Date}        dateObj
- * @param  {boolean}     showSecs
- * @return {string}      theTime
- **/
-function get24HrTime(dateObj, showSecs) {
-  var hours = leadZero(dateObj.getHours());
-  var mins = leadZero(dateObj.getMinutes());
-  var secs = leadZero(dateObj.getSeconds());
-  var theTime;
-
-  theTime = hours + ":" + mins;
-  if(showSecs === true) {theTime += ":" + secs}
-  return theTime;
-}
-
-/**
- * Add a leading zero is needed
- * @param  {number} number
- * @return {string}
- **/
-function leadZero(number) {
-  return (number < 10) ? "0" + number : number;
-}
-
-/**
- * Returns the date in the format DD/MM/YYYY
- * @param  {Date}    dateObj
- * @return {string}
- **/
-function getShortDate(dateObj) {
-  var date = leadZero(dateObj.getDate());
-  var month = leadZero(dateObj.getMonth() + 1);
-  var year = dateObj.getFullYear();
-  return date + "/" + month + "/" + year;
-}
-
-/**
- * Return the date in the format Day Date Month Year
- * @param  {Date}    dateObj
- * @return {string}
- **/
-function getLongDate(dateObj) {
-  var day = textifyWeekDay(dateObj.getDay());
-  var date = suffixDate(dateObj.getDate());
-  var month = textifyMonth(dateObj.getMonth());
-  var year = dateObj.getFullYear();
-  return day + " " + date + " " + month + " " + year;
-}
-
-/**
- * Add a suffix to the date
- * @param  {number} date
- * @return {string} theDate
- **/
-function suffixDate(date) {
-  var theDate;
-  switch(date) {
-    case 1:
-    case 21:
-    case 31:
-      theDate = date + "st";
-      break;
-    case 2:
-    case 22:
-    case 32:
-      theDate = date + "nd";
-      break;
-    case 3:
-    case 23:
-      theDate = date + "rd";
-      break;
-    default:
-      theDate = date + "th";
+var dateTime = (function () {
+  /**
+   * Convert the month number into text
+   * @param  {number} month
+   * @return {string}
+   **/
+  function textifyMonth(month) {
+    var months = [
+      "January",
+      "Febuary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    return months[month];
   }
-  return theDate;
-}
+
+  /**
+   * Convert the day number into text
+   * @param  {number} day
+   * @return {string}
+   **/
+  function textifyWeekDay(day) {
+    var days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    return days[day];
+  }
+
+  /**
+   * Returns the time in 12hr format
+   * @param  {Date}        dateObj
+   * @param  {boolean}     showSecs
+   * @param  {boolean}     showAMPM
+   * @return {string}      theTime
+   **/
+  function get12HrTime(dateObj, showSecs, showAMPM) {
+    var hours = (dateObj.getHours() > 12) ? dateObj.getHours() - 12 : dateObj.getHours();
+    var mins = leadZero(dateObj.getMinutes());
+    var secs = leadZero(dateObj.getSeconds());
+    var ampm = (dateObj.getHours() > 12) ? "PM" : "AM";
+    var theTime;
+
+    theTime = hours + ":" + mins;
+    if(showSecs === true) {theTime += ":" + secs}
+    if(showAMPM === true) {theTime += " " + ampm}
+    return theTime;
+  }
+
+  /**
+   * Returns the time in 24hr format
+   * @param  {Date}        dateObj
+   * @param  {boolean}     showSecs
+   * @return {string}      theTime
+   **/
+  function get24HrTime(dateObj, showSecs) {
+    var hours = leadZero(dateObj.getHours());
+    var mins = leadZero(dateObj.getMinutes());
+    var secs = leadZero(dateObj.getSeconds());
+    var theTime;
+
+    theTime = hours + ":" + mins;
+    if(showSecs === true) {theTime += ":" + secs}
+    return theTime;
+  }
+
+  /**
+   * Add a leading zero is needed
+   * @param  {number} number
+   * @return {string}
+   **/
+  function leadZero(number) {
+    return (number < 10) ? "0" + number : number;
+  }
+
+  /**
+   * Returns the date in the format DD/MM/YYYY
+   * @param  {Date}    dateObj
+   * @return {string}
+   **/
+  function getShortDate(dateObj) {
+    var date = leadZero(dateObj.getDate());
+    var month = leadZero(dateObj.getMonth() + 1);
+    var year = dateObj.getFullYear();
+    return date + "/" + month + "/" + year;
+  }
+
+  /**
+   * Return the date in the format Day Date Month Year
+   * @param  {Date}    dateObj
+   * @return {string}
+   **/
+  function getLongDate(dateObj) {
+    var day = textifyWeekDay(dateObj.getDay());
+    var date = suffixDate(dateObj.getDate());
+    var month = textifyMonth(dateObj.getMonth());
+    var year = dateObj.getFullYear();
+    return day + " " + date + " " + month + " " + year;
+  }
+
+  /**
+   * Add a suffix to the date
+   * @param  {number} date
+   * @return {string} theDate
+   **/
+  function suffixDate(date) {
+    var theDate;
+    switch(date) {
+      case 1:
+      case 21:
+      case 31:
+        theDate = date + "st";
+        break;
+      case 2:
+      case 22:
+      case 32:
+        theDate = date + "nd";
+        break;
+      case 3:
+      case 23:
+        theDate = date + "rd";
+        break;
+      default:
+        theDate = date + "th";
+    }
+    return theDate;
+  }
+
+  return {
+    textifyMonth:     function (month)                              { return textifyMonth(month) },
+    textifyWeekDay:   function (day)                                { return textifyWeekDay(day) },
+    get12HrTime:      function (dateObj, showSecs, showAMPM)        {  return get12HrTime(dateObj, showSecs, showAMPM) },
+    get24HrTime:      function (dateObj, showSecs)                  { return get24HrTime(dateObj, showSecs) },
+    leadZero:         function (number)                             { return leadZero(number) },
+    getShortDate:     function (dateObj)                            { return getShortDate(dateObj) },
+    getLongDate:      function (dateObj)                            { return getLongDate(dateObj) },
+    suffixDate:       function (date)                               { return suffixDate(date) }
+  }
+}) ();
 
 ////////////////////////////////////////////////////////////////
