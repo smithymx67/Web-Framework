65e8468fd86f8450b55047574c1f4a715c6e99f2
Sam Smith <smithymx67@gmail.com>
2018-06-08 11:44:25 +0100

    Made copyright date dynamic

--- src/docSite/includes/footer.php
+++ src/docSite/includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer">
   <div class="container">
     <div class="col-6 col-s-12 center-align-s">
-      <p>&copy; <a href="https://samsmith.me" target="_blank">Sam Smith</a> 2017</p>
+      <p>&copy; <a href="https://samsmith.me" target="_blank">Sam Smith</a> <?php echo date('Y') ?></p>
     </div>
     <div class="col-6 col-s-12 right-align center-align-s">
       <p>Version <?php echo VERSION ?></p>
